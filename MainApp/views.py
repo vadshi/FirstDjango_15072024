9006ee5b559caa1e3f2fdcc2be2102f987bedcc4
Vadim <5208643@mail.ru>
2024-07-16 18:51:00 +0300

    Done. M1:task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+}
+
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
     <strong>Автор</strong>: <i>Иванов И.П.</i>
+    """
+    return HttpResponse(text)
+
+
+def about(request):
+    text = f"""
+    Имя: {author["Имя"]}<br>
+    Отчество: {author["Отчество"]}<br>
+    Фамилия: {author["Фамилия"]}<br>
+    телефон: {author['телефон']}<br>
+    email: {author["email"]}
     """
     return HttpResponse(text)
